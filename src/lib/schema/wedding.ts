54aa4f2df561af6d946ad1c8d17763130b329faa
AJ Caldwell <tomatrow@gmail.com>
2024-02-04 20:12:30 -0800

    Update airtable schema

--- src/lib/schema/wedding.ts
+++ src/lib/schema/wedding.ts
@@ -1,9 +1,19 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
-export const PeopleAttendance = z.enum(['Attending', 'Declined']);
+export const PeopleAttendance = z.enum([
+  'Attending',
+  'Declined',
+])
+
+export const PeopleMeal = z.enum([
+  'Beef Stew',
+  'Lentil Soup',
+])
 
 export const PeopleSchema = z.object({
-	Name: z.string().optional(),
-	Attendance: PeopleAttendance.optional()
-});
-export type People = z.infer<typeof PeopleSchema>;
+  'Name': z.string().optional(),
+  'Attendance': PeopleAttendance.optional(),
+  'Meal': PeopleMeal.optional(),
+  'Signup Time': z.coerce.date().optional(),
+})
+export type People = z.infer<typeof PeopleSchema>
